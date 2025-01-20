c5b2494b70e27345bb05ae2997fe77886e61be35
mihiraryaa <150774823+mihiraryaa@users.noreply.github.com>
2025-01-20 14:52:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from templates import *
+from agent_templates import *
 from schemas import *
 from prompts import * 
 import time
@@ -6,11 +6,13 @@ from langgraph.checkpoint.memory import MemorySaver
 memory=MemorySaver()
 from langchain_community.tools.tavily_search import TavilySearchResults
 tavily_tool = TavilySearchResults(
-    max_results=3,          
-    search_depth="advanced", 
+    max_results=3,          # Maximum number of results to return
+    search_depth="advanced", # Level of search depth
 )
 
-
+'''
+company_research_agent, project_research_agent,  marketing_strategy_report,  generate campaigns adhering report, content creation
+'''
 def call_model(prompt, model=model_llama):
     res=model.invoke(prompt)
     return res
@@ -30,7 +32,7 @@ def company_research(state:AgentState):
     inputs=state["inputs"]
     research=state["research"]
     system=SystemMessage(content=company_prompt)
-    user=HumanMessage(content=f"#Company name:\n{inputs["name"]}\n\n#Project description:\n{inputs["project_des"]}")
+    user=HumanMessage(content=f"#Company name:\n{inputs["company_name"]}\n\n#Project description:\n{inputs["project_description"]}")
 
     res=research_agent([system,user])
     research["company_report"]=res
@@ -42,7 +44,7 @@ def project_research(state:AgentState):
     inputs=state["inputs"]
     research=state["research"]
     system=SystemMessage(content=project_prompt)
-    user=HumanMessage(content=f"##Company report:\n{research['company_report']}\n\n\n##Project description:\n{inputs["project_des"]} ")
+    user=HumanMessage(content=f"##Company report:\n{research['company_report']}\n\n\n##Project description:\n{inputs["project_description"]} ")
     prompt=[system,user]
     res=research_agent(prompt)
     research["project_understanding"]=res
@@ -63,7 +65,7 @@ def campaign_ideas_agent(state:AgentState):
     user=HumanMessage(content=f"#COMPANY REPORT:\n {research['company_report']}\n\n\n#PROJECT UNDERSTANDING:\n{research['project_understanding']}\n\n\n#MARKETING STRATEGY:\n{strategy}")
     model=model_llama.with_structured_output(campaign_ideas)
     ideas=model.invoke([system,user])
-    #print(ideas)
+    print(ideas)
     return {"ideas": ideas.ideas}
 
 def campaign_planner_agent(state:AgentState):
@@ -73,7 +75,7 @@ def campaign_planner_agent(state:AgentState):
     campaigns=[]
     system=SystemMessage(content=campaign_planner_prompt)
     for idea in ideas:
-        user=HumanMessage(content=f"#COMPANY REPORT:\n {research['company_report']}\n\n\n#PROJECT UNDERSTANDING:\n{research['project_understanding']}\n\n\n#MARKETING STRATEGY:\n{strategy}\n\n\n#CAMPAIGN IDEA:\n{idea}")
+        user=HumanMessage(content=f"#MARKETING STRATEGY:\n{strategy}\n\n\n#CAMPAIGN IDEA:\n{idea}")
         model=model_llama.with_structured_output(campaign)
         res=research_agent([system,user])
         campaigns.append(res)
@@ -85,10 +87,10 @@ def content_creation_agent(state:AgentState):
     system=SystemMessage(content=content_creation_prompt)
     contents=[]
     for camp in campaigns:
-        user=HumanMessage(content=f"#COMPANY REPORT:\n {report}\n\n\n#CAMPAIGN PLAN:\n{camp}")
+        user=HumanMessage(content=f"#CAMPAIGN PLAN:\n{camp}")
         content=model_llama.invoke([system,user])
         contents.append(content.content)
-        #print(content.content)
+        print(content.content)
     
     return {"contents": contents}
 
@@ -96,7 +98,7 @@ def write(strategy:str, campaigns:list[str], contents:list[str]):
     file_path="outputs/Marketing_strategy.md"
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(strategy)
-        
+    nos=1
     for nos in range(len(contents)):
         file_path=f"outputs/campaigns/campaign_{nos+1}.md"
         with open(file_path, "w",encoding="utf-8") as f:
@@ -114,7 +116,13 @@ def output(state: AgentState):
     write(strategy,campaigns, contents)
     print(f"company report:\n{comp_report}\n\n\n\nProject Understanding:\n{project_understanding}")
     print(f'\n\n\nMarketing Strategy: \n{strategy}')
-    
+    print("\n\n\n\nCampaigns:\n\n")
+
+    for event in campaigns:
+        print(event)
+        print('#'*150)
+    return {"output": ""}
+
 
 
 
@@ -141,10 +149,8 @@ workflow=workflow.compile()
 
 input={"inputs": user_query, "research": {}}
 
-
 for event in workflow.stream(input):
     print(event)
     print('-'*165)
-
 
 
